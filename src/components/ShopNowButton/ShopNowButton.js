fac0114469d94b77cfb605ffbbf5a0216bcc4ead
Stephen Di Rienzo <stephen.dirienzo@gmail.com>
2022-06-25 17:26:43 -0400

    Introduce Shop Now button component

---
+++ src/components/ShopNowButton/ShopNowButton.js
@@ -0,0 +1,9 @@
+import Button from "../Button/Button";
+
+function ShopNowButton() {
+    return (
+        <Button classList={'btn-primary'} text={'Shop Now'} />
+    );
+}
+
+export default ShopNowButton;
