632486d25b8b1ae12e4f1fae171f2df81e51722a
Stephen Di Rienzo <stephen.dirienzo@gmail.com>
2022-06-26 12:00:08 -0400

    Change App container from div to main to improve semantics

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import Home from './components/Home/Home';
 function App() {
 
   return (
-    <div className="App">
+    <main className="App">
         <Home />
-    </div>
+    </main>
   );
 }
 
