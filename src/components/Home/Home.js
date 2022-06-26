e553ec1fe3f88bbb5421049d7d471a1c1d91b248
Stephen Di Rienzo <stephen.dirienzo@gmail.com>
2022-06-26 12:00:36 -0400

    Add the About section to the Home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import About from '../About/About';
 import Header from '../Header/Header';
 import Jumbotron from '../Jumbotron/Jumbotron';
 import './Home.module.css';
@@ -8,6 +9,7 @@ function Home() {
         <>
             <Header />
             <Jumbotron />
+            <About />
         </>
     );
 }
