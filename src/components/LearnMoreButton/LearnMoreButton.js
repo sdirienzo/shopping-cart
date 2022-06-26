c845adbe3b5c0a14e1f8b54998b0c12690f4454b
Stephen Di Rienzo <stephen.dirienzo@gmail.com>
2022-06-26 11:58:22 -0400

    Introduce the Learn More button component

---
+++ src/components/LearnMoreButton/LearnMoreButton.js
@@ -0,0 +1,9 @@
+import Button from "../Button/Button";
+
+function LearnMoreButton() {
+    return (
+        <Button classList={'btn-primary'} text={'Learn More'} />
+    );
+}
+
+export default LearnMoreButton;
