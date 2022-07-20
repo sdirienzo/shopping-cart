dd34deae5ebbdcda4db1ea657f177f28bc43d9c3
Stephen Di Rienzo <stephen.dirienzo@gmail.com>
2022-07-19 21:18:30 -0400

    Replace Home and Shop with product categories and configure their respective link routes

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -5,9 +5,20 @@ function Nav() {
     return (
         <nav className={styles.nav}>
             <ul className={styles.navLinks}>
-                <li className={styles.navLink}>Home</li>
                 <li className={styles.navLink}>
-                    <Link to='/shop/chairs'>Shop</Link>
+                    <Link to='/shop/sofas'>Sofas</Link>
+                </li>
+                <li className={styles.navLink}>
+                    <Link to='/shop/chairs'>Chairs</Link>
+                </li>
+                <li className={styles.navLink}>
+                    <Link to='/shop/coffee-tables'>Coffee Tables</Link>
+                </li>
+                <li className={styles.navLink}>
+                    <Link to='/shop/dressers'>Dressers</Link>
+                </li>
+                <li className={styles.navLink}>
+                    <Link to='/shop/lamps'>Lamps</Link>
                 </li>
                 <li className={styles.navLink}>Cart</li>
             </ul>
