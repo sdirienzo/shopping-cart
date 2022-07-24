bbf0081252079307e8ec08e3a9bed5f55a994bbc
Stephen Di Rienzo <stephen.dirienzo@gmail.com>
2022-07-24 12:41:46 -0400

    Add link to collection item so users are taken to respective product page when they select a collection item in featured or popular sections of home page

--- src/components/CollectionItem/CollectionItem.js
+++ src/components/CollectionItem/CollectionItem.js
@@ -1,10 +1,13 @@
+import { Link } from 'react-router-dom';
 import styles from './CollectionItem.module.css';
 
-function CollectionItem({ name, price, type, imageUrl }) {
+function CollectionItem({ id, name, price, type, imageUrl }) {
     return (
         <article className={styles.collectionItem}>
             <div className={styles.imageContainer}>
-                <img className={styles.image} src={require(`../../assets/images${imageUrl}`)} alt={type} />
+                <Link to={`/product/${id}`}>
+                    <img className={styles.image} src={require(`../../assets/images${imageUrl}`)} alt={type} />
+                </Link>
             </div>
             <p className={styles.name}>{name}</p>
             <p className={styles.price}>{`$${price}`}</p>
