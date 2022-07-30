adc9e9967199c0f2f76cdea45560fc0d39c066a8
Stephen Di Rienzo <stephen.dirienzo@gmail.com>
2022-07-30 17:04:54 -0400

    Calculate and display cart total

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,15 +1,31 @@
+import { useEffect, useState } from 'react';
 import CartItem from '../CartItem/CartItem';
 import styles from './Cart.module.css';
 
 function Cart({ cart, onAdd, onRemove }) {
+    const [total, setTotal] = useState(0);
+
+    useEffect(function calculateTotal() {
+        const newTotal = cart.reduce(function (prevValue, currItem) {
+            return prevValue + (currItem.itemDetails.price * currItem.quantity);
+        }, 0);
+
+        setTotal(newTotal);
+    }, [cart]);
 
     return (
         <main className={styles.cart}>
             <h3 className={styles.cartHeading}>Shopping Cart</h3>
             <section className={styles.cartItems}>
                 {cart.map(item => (
-                    <CartItem key={item.item.id} {...item} />
+                    <CartItem key={item.itemDetails.id} {...item} />
                 ))}
+            </section>
+            <section className={styles.cartCheckout}>
+                <div className={styles.cartTotalContainer}>
+                    <span className={styles.cartTotalLabel}>Total:</span>
+                    <span className={styles.cartTotal}>${total}</span>
+                </div>
             </section>
         </main>
     )
